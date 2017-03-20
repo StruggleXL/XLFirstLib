7ddd852951463e0c64a5b6ce55588501efe7b29e
xulong <long.xu@srgroup.cn>
2017-03-20 17:47:22 +0800

    添加测试工程

---
+++ XLFirstLibDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XLFirstLibDemo
+//
+//  Created by xl on 17/3/20.
+//  Copyright © 2017年 xl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XLFirstLibDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XLFirstLibDemo
+//
+//  Created by xl on 17/3/20.
+//  Copyright © 2017年 xl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
